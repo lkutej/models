82209b8e866e2ab7e9a04dda4f947a170baff091
lkutej <lukas.kutej@gmx.de>
2024-02-09 16:56:28 +0100

    kZetaF.C

--- compressibleVLES/kZetaF/kZetaF.C
+++ compressibleVLES/kZetaF/kZetaF.C
@@ -23,101 +23,68 @@ License
 
 \*---------------------------------------------------------------------------*/
 
-#include "zetaF.H"
+#include "kZetaF.H"
 #include "addToRunTimeSelectionTable.H"
 #include "wallFvPatch.H"
+#include "mappedWallFvPatch.H"
+
 //#include "backwardsCompatibilityWallFunctions.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 namespace Foam
 {
-namespace incompressible
+namespace compressible
 {
 namespace RASModels
 {
 
 // * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
 
-defineTypeNameAndDebug(zetaF, 0);
-addToRunTimeSelectionTable(RASModel, zetaF, dictionary);
+defineTypeNameAndDebug(kZetaF, 0);
+addToRunTimeSelectionTable(RASModel, kZetaF, dictionary);
 
 // * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * * //
 
-tmp<volScalarField> zetaF::Tau() const
+tmp<volScalarField> kZetaF::Tau() const
 {
-/*    
-    volScalarField T_lb("T_lb", CTau_*sqrt(nu()/(epsilon_+epsilonMin_)));
-    volScalarField T_ub("T_ub", a_/((sqrt(6.0)*Cmu_*mag(symm(fvc::grad(U_))))*zeta_+mTSmall_));
-    volScalarField T_nb("T_nb",  k_/(epsilon_+epsilonMin_));
-    
-    dimensionedScalar TSmall("TSmall", dimTime, 1e-15);
-    
-    volScalarField v_min = min(T_nb, T_ub);
-    volScalarField I_min = 1.0*pos(T_nb - T_ub - TSmall); // = 1 wenn gebounded
-    volScalarField I_max = 2.0*pos(T_lb - v_min - TSmall); // = 1 wenn gebounded
-    volScalarField TInd("TInd", I_min + I_max);
-    
-    if (runTime_.outputTime())
-    {
-        TInd.write();
-        T_lb.write();
-        T_ub.write();
-        T_nb.write();
-    }
-*/   
-    return max(
-                  min(
-                           k_/(epsilon_+epsilonMin_),
-                                 ( a_/((sqrt(6.0)*Cmu_*mag(symm(fvc::grad(U_))))*zeta_+mTSmall_))
-                       ),
-                   CTau_*sqrt(nu()/(epsilon_+epsilonMin_))
-               );
+    return max
+           (
+               min
+               (
+                   k_/(epsilon_+epsilonMin_),
+                   a_/((sqrt(6.0)*Cmu_*mag(symm(fvc::grad(U_))))*zeta_+mTSmall_)    //BK: Durbin Original 1/(sqrt(6) zeta Cmu sqrt(Sij Sij)
+               ),
+               CTau_*sqrt((mu()/rho_)/(epsilon_+epsilonMin_))
+           ); 
 }
 
-tmp<volScalarField> zetaF::L() const
+tmp<volScalarField> kZetaF::L() const
 {
-/*
-    volScalarField L_lb("L_lb", CEta_*pow( (pow(nu(),3)/(epsilon_+epsilonMin_)),0.25));
-    volScalarField L_ub("L_ub",sqrt(k_)/((sqrt(6.0)*Cmu_*mag(symm(fvc::grad(U_))))*zeta_));
-    volScalarField L_nb("L_nb",pow(k_,1.5)/(epsilon_+epsilonMin_));
-    
-    dimensionedScalar LSmall("LSmall", dimLength, 1e-15);
-    
-    volScalarField v_min = min(L_nb, L_ub);
-    volScalarField I_min = 1.0*pos(L_nb - L_ub - LSmall); // = 1 wenn gebounded
-    volScalarField I_max = 2.0*pos(L_lb - v_min - LSmall); // = 1 wenn gebounded
-    volScalarField LInd("LInd", I_min + I_max);
-    
-    if (runTime_.outputTime())
-    {
-        LInd.write();
-        L_lb.write();
-        L_ub.write();
-        L_nb.write();
-    }
-*/
-    return CL_*max(
-                      min(
-                              pow(k_,1.5)/(epsilon_+epsilonMin_),
-                                sqrt(k_)/((sqrt(6.0)*Cmu_*mag(symm(fvc::grad(U_))))*zeta_)
-                          ),
-                      CEta_*pow( (pow(nu(),3)/(epsilon_+epsilonMin_)),0.25)
-                  );
+    return CL_*max
+               (
+                   min                                                              //BK: in Samules Version auskommentiert
+                   (                                                                //BK: in Samules Version auskommentiert
+                       pow(k_,1.5)/(epsilon_+epsilonMin_),
+                       sqrt(k_)/((sqrt(6.0)*Cmu_*mag(symm(fvc::grad(U_))))*zeta_)   //BK: in Samules Version auskommentiert
+                   ),                                                               //BK: in Samules Version auskommentiert
+                   CEta_*pow( (pow((mu()/rho_),3)/(epsilon_+epsilonMin_)),0.25)
+               );
 }
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-zetaF::zetaF
+kZetaF::kZetaF
 (
+    const volScalarField& rho,
     const volVectorField& U,
     const surfaceScalarField& phi,
-    transportModel& transport,
+    const fluidThermo& thermophysicalModel,
     const word& turbulenceModelName,
     const word& modelName
 )
 :
-    RASModel(modelName, U, phi, transport, turbulenceModelName),
+    RASModel(modelName, rho, U, phi, thermophysicalModel, turbulenceModelName),
 
     Cmu_
     (
@@ -128,13 +95,22 @@ zetaF::zetaF
             0.22
         )
     ),
-    CEps2_
+    Ceps2_
     (
         dimensioned<scalar>::lookupOrAddToDict
         (
             "Ceps2",
             coeffDict_,
             1.9
+        )
+    ),
+    Ceps3_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "Ceps3",
+            coeffDict_,
+            -0.33
         )
     ),
     C1_
@@ -219,6 +195,16 @@ zetaF::zetaF
         )
     ),
 
+    Prt_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "Prt",
+            coeffDict_,
+            1.0
+        )
+    ),
+    
     k_
     (
         IOobject
@@ -270,12 +256,12 @@ zetaF::zetaF
         ),
         mesh_
     ),
-
-    nut_
+    
+    mut_
     (
         IOobject
         (
-            "nut",
+            "mut",
             runTime_.timeName(),
             mesh_,
             IOobject::MUST_READ,
@@ -283,39 +269,44 @@ zetaF::zetaF
         ),
         mesh_
     ),
-   
-    yr_(mesh_),
 
-    mTSmall_
+    alphat_
     (
-	"mTSmall",	
-	dimensionSet(0, 0, -1, 0, 0, 0, 0),
-	1e-10
+        IOobject
+        (
+            "alphat",
+            runTime_.timeName(),
+            mesh_,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh_
     ),
-	
-    zetaMin_
-    (
-        "zetaMin",
-         dimless,
-         SMALL
-    ),
-    fMin_
-    (
-        "fMin",
-        dimless/dimTime,
-        SMALL
-    )
+
+    yr_(mesh_),
+    
+    zetaMin_("zetaMin", dimless, SMALL),
+    fMin_("fMin", dimless/dimTime, SMALL),
+    mTSmall_("mTSmall", dimless/dimTime, SMALL)
 {
     bound(k_, kMin_);
     bound(epsilon_, epsilonMin_);
+    bound(f_, fMin_);
     bound(zeta_, zetaMin_);
+    
+    alphat_ = mut_/Prt_;
+    alphat_.correctBoundaryConditions();
+    
+    mut_ = rho_*Cmu_*zeta_*k_*Tau();
+    mut_.correctBoundaryConditions();
+    
     printCoeffs();
 }
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
 
-tmp<volSymmTensorField> zetaF::R() const
+tmp<volSymmTensorField> kZetaF::R() const
 {
     return tmp<volSymmTensorField>
     (
@@ -329,14 +320,14 @@ tmp<volSymmTensorField> zetaF::R() const
                 IOobject::NO_READ,
                 IOobject::NO_WRITE
             ),
-            ((2.0/3.0)*I)*k_ - nut_*twoSymm(fvc::grad(U_)),
+            ((2.0/3.0)*I)*k_ - (mut_/rho_) * twoSymm(fvc::grad(U_)), //BK: Scheint korrekt
             k_.boundaryField().types()
         )
     );
 }
 
 
-tmp<volSymmTensorField> zetaF::devReff() const
+tmp<volSymmTensorField> kZetaF::devRhoReff() const
 {
     return tmp<volSymmTensorField>
     (
@@ -350,42 +341,27 @@ tmp<volSymmTensorField> zetaF::devReff() const
                 IOobject::NO_READ,
                 IOobject::NO_WRITE
             ),
-           -nuEff()*dev(twoSymm(fvc::grad(U_)))
+           -muEff()*dev(twoSymm(fvc::grad(U_))) //BK: Scheint korrekt
         )
     );
 }
 
 
-tmp<fvVectorMatrix> zetaF::divDevReff(volVectorField& U) const
+tmp<fvVectorMatrix> kZetaF::divDevRhoReff(volVectorField& U) const
 {
     return
     (
-      - fvm::laplacian(nuEff(), U)
-      - fvc::div(nuEff()*dev(fvc::grad(U)().T()))
-    );
+      - fvm::laplacian(muEff(), U)
+      - fvc::div(muEff()*dev2(T(fvc::grad(U))))
+    ); //BK: Scheint korrekt
 }
 
-tmp<fvVectorMatrix> zetaF::divDevRhoReff
-(
-    const volScalarField& rho,
-    volVectorField& U
-) const
-{
-    volScalarField muEff("muEff", rho*nuEff());
-
-    return
-    (
-      - fvm::laplacian(muEff, U)
-      - fvc::div(muEff*dev(T(fvc::grad(U))))
-    );
-}
-
-bool zetaF::read()
+bool kZetaF::read()
 {
     if (RASModel::read())
     {
         Cmu_.readIfPresent(coeffDict());
-        CEps2_.readIfPresent(coeffDict());
+        Ceps2_.readIfPresent(coeffDict());
         C1_.readIfPresent(coeffDict());
         C2_.readIfPresent(coeffDict());
         sigmaK_.readIfPresent(coeffDict());
@@ -405,7 +381,7 @@ bool zetaF::read()
 }
 
 
-void zetaF::correct()
+void kZetaF::correct()
 {
     RASModel::correct();
 
@@ -414,87 +390,115 @@ void zetaF::correct()
         return;
     }
 
-    volScalarField G("RASModel::G", nut_*2*magSqr(symm(fvc::grad(U_))));
-    volScalarField CEps1_ = 1.4*(1.0+(0.012/(zeta_+zetaMin_)));
-
+    volScalarField divU(fvc::div(phi_/fvc::interpolate(rho_)));
+    
+    if (mesh_.moving())
+    {
+        divU += fvc::div(mesh_.phi());
+    }
+    
+    const volTensorField gradU(fvc::grad(U_));
+    const volScalarField S2(2*magSqr(dev(symm(gradU)))); //BK: S2 = 2 Sij Sij - 2/3 Skk Skk; 2/3 k vernachlässigt
+    const volScalarField G(GName(), mut_*S2);
+    //volScalarField G("RASModel::G", mut_*2*magSqr(symm(fvc::grad(U_)))); //BK: symm(gradU) oder dev(symm(gradU))???
+    
+    volScalarField Ceps1_ = 1.4*(1.0+(0.012/zeta_));
+    
     volScalarField T_ = Tau();
     volScalarField L_ = L();
+    
 
+    // Update epsilon (and possibly G) at the wall
+    //epsilon_.boundaryField().updateCoeffs();
+     
     // Dissipation equation
     tmp<fvScalarMatrix> epsEqn
     (
-        fvm::ddt(epsilon_)
+        fvm::ddt(rho_, epsilon_)
       + fvm::div(phi_, epsilon_)
-      - fvm::Sp(fvc::div(phi_), epsilon_)
       - fvm::laplacian(DepsilonEff(), epsilon_)
      ==
-        CEps1_*G/T_
-      - fvm::Sp(CEps2_/T_, epsilon_)
+        Ceps1_*G/T_
+      - fvm::Sp(rho_*Ceps2_/T_, epsilon_) //BK: Dichte
 /*
-	CEps1_*G*epsilon_/k_
-      - fvm::Sp(CEps2_*epsilon_/k_, epsilon_)
+        Ceps1_*G*epsilon_/k_
+      - fvm::Sp(Ceps2_*epsilon_/k_, epsilon_)
 */
+      - fvm::SuSp(((2.0/3.0)*Ceps1_ + Ceps3_)*rho_*divU, epsilon_) //BK: Kompressibilität?!
     );
 
     epsEqn().relax();
     //epsEqn().boundaryManipulate(epsilon_.boundaryField());
-
-    dimensionedScalar nu1 = nu()->internalField()[0];
+   
+    //dimensionedScalar rho1 = rho_[0];
+    //dimensionedScalar mu1 = mu()[0];
+    //dimensionedScalar nu1 = mu1/rho1; Info<<"WARNING: Only for rhoConst, ";
+    //dimensionedScalar nu1("nu1", dimensionSet(0, 2, -2, 0, 0, 0, 0), 2e-5); 
     #include "BoundaryConditionEp.H"
-
     solve(epsEqn);
     bound(epsilon_, epsilonMin_);
 
-
+    
     // Turbulent kinetic energy equation
     tmp<fvScalarMatrix> kEqn
     (
-        fvm::ddt(k_)
+        fvm::ddt(rho_, k_)
       + fvm::div(phi_, k_)
-      - fvm::Sp(fvc::div(phi_), k_)
       - fvm::laplacian(DkEff(), k_)
      ==
         G
-      - fvm::Sp(epsilon_/k_, k_)
-     );
+      - fvm::Sp(rho_*epsilon_/k_, k_) //BK: Dichte
+      - fvm::SuSp((2.0/3.0)*rho_*divU, k_) //BK: Kompressibilität?!
+    );
 
     kEqn().relax();
     solve(kEqn);
-    bound(k_, kMin_);
+    //bound(k_, kMin_);
+    dimensionedScalar kSmall_("kSmall", dimensionSet(0, 2, -2, 0, 0, 0, 0), 1e-10);
+    bound(k_, kSmall_);
+
 
     // f equation
     tmp<fvScalarMatrix> fEqn
     (
-        - fvm::laplacian(f_)
+        - fvm::laplacian(rho_, f_)
      ==
-        - fvm::Sp(1.0/sqr(L_),f_)
-        - (C1_+(C2_*G/(epsilon_)))*((zeta_ - 2.0/3.0))/(sqr(L_)*T_)
+        - fvm::Sp(rho_/sqr(L_),f_) //BK: Dichte
+        - (rho_*C1_+C2_*G/epsilon_)*(zeta_ - 2.0/3.0)/(sqr(L_)*T_) //BK: Dichte
     );
+    
     fEqn().relax();
     solve(fEqn);
     bound(f_, fMin_);
 
-    volScalarField fTilda = f_ - 2.0*nu()*zeta_/sqr(yr_);
+    
+    // Calculate f from the transformed fEqn
+    volScalarField fTilda = f_ - 2.0*(mu()/rho_)*zeta_/sqr(yr_);
 
-    // zeta equation
+    
+    // Zeta equation
     tmp<fvScalarMatrix> zetaEqn
     (
-        fvm::ddt(zeta_)
+        fvm::ddt(rho_, zeta_)
       + fvm::div(phi_, zeta_)
-      - fvm::Sp(fvc::div(phi_), zeta_)
       - fvm::laplacian(DzetaEff(), zeta_)
      ==
-        fTilda
+//      f_
+        rho_*fTilda //BK: Dichte
       - fvm::Sp(G/(k_), zeta_)
+//    - fvm::Sp(G/(k_+kSmall_), zeta_)
     );
 
     zetaEqn().relax();
     solve(zetaEqn);
     bound(zeta_, zetaMin_);
-
+    zeta_ = min(zeta_, 2.0); //BK: Warum maximal 1.5?
+    
     // Re-calculate viscosity
-    nut_ = Cmu_*zeta_*k_*T_;
-    nut_.correctBoundaryConditions();
+    mut_ = rho_*Cmu_*zeta_*k_*T_; //BK: Dichte
+    //mut_.correctBoundaryConditions();
+
+    alphat_ = mut_/Prt_;
 }
 
 
